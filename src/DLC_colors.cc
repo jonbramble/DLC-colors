04612eb0364837ea09df4f6db66da3c11c858643
Jonathan Bramble <phyjpb@leeds.ac.uk>
2011-10-24 22:23:27 +0100

    removed useless fopenmp code

--- src/DLC_colors.cc
+++ src/DLC_colors.cc
@@ -3,16 +3,17 @@
 void DLC::Michel_levy(double Dn, int dlen, int dstart, int dend, gsl_matrix * Xmat, gsl_matrix * Ymat, gsl_matrix * Zmat)
 {
 	const int len = 471; // data length defined by cie data
-	const int ypix=100; // this will be replaced by rho
+	const int ypix= 100; // this will be replaced by rho
 	int yp;
 
  	double dstep, X, Y, Z, Xn, Yn, Zn, Norm, DeltaComp, DeltaSamp, cDC2, sDC2, cDS2, sDS2, crho, srho, Ts, xmi, ymi, zmi, R, G, B;
 	const double lambda0 = 528;   // for my compensator this is correct
 	const int m = 4;		// this is an approximation to a real device
 
-	int tid, nthreads; // threading
+	int rhol, dc, l;
 
-	const double rho = M_PI/4;
+	const double rho_max = M_PI/4;
+	int rhostep = (2*rho_max)/ypix;
 
 	gsl_complex one = gsl_complex_rect(0,1);
 	gsl_complex zero = gsl_complex_rect(0,0);
@@ -25,11 +26,14 @@ void DLC::Michel_levy(double Dn, int dlen, int dstart, int dend, gsl_matrix * Xm
 	gsl_matrix_complex * Comp = gsl_matrix_complex_alloc (2,2);
 	gsl_matrix_complex * Sample = gsl_matrix_complex_alloc (2,2);
 	gsl_matrix_complex * Reflect = gsl_matrix_complex_alloc (2,2);
-	gsl_matrix_complex * Temp = gsl_matrix_complex_alloc (2,2);
+	gsl_matrix_complex * TempA = gsl_matrix_complex_alloc (2,2);
+	gsl_matrix_complex * TempB = gsl_matrix_complex_alloc (2,2);
 
 	gsl_complex comp_c[2];
 	gsl_complex sample_c[4];
 	gsl_complex refl_c[4];
+
+	gsl_complex Tx, Ty;
 
 	gsl_vector_complex_set(A,0,zero);
 	gsl_vector_complex_set(A,1,one);
@@ -41,6 +45,7 @@ void DLC::Michel_levy(double Dn, int dlen, int dstart, int dend, gsl_matrix * Xm
 
 	gsl_vector * d = gsl_vector_alloc (dlen);
 	gsl_vector * lambda = gsl_vector_alloc (len);
+	gsl_vector * rho = gsl_vector_alloc (ypix);
 
 	gsl_vector * Xr = gsl_vector_alloc (len);
 	gsl_vector * Yr = gsl_vector_alloc (len);
@@ -61,11 +66,17 @@ void DLC::Michel_levy(double Dn, int dlen, int dstart, int dend, gsl_matrix * Xm
 		gsl_vector_set(lambda,i,360+i);
 	}
 
+	for(int i=0;i<ypix;i++)
+	{
+		gsl_vector_set(rho,i,(-1*rho_max)+rhostep*i);
+	}
+
 	// load cie data
   	gsl_vector * xm = gsl_vector_alloc (len);
 	gsl_vector * ym = gsl_vector_alloc (len);
 	gsl_vector * zm = gsl_vector_alloc (len);
   
+	//should check these are present
 	FILE * fxm = fopen("xm.txt", "r");
 	FILE * fym = fopen("ym.txt", "r");
 	FILE * fzm = fopen("zm.txt", "r");
@@ -77,134 +88,92 @@ void DLC::Michel_levy(double Dn, int dlen, int dstart, int dend, gsl_matrix * Xm
 	fclose(fxm);
 	fclose(fym);
 	fclose(fzm);
+	// end of file loading
 
-
-	for (int dc=0;dc<dlen;dc++)
+	for (rhol=0;rhol<ypix;rhol++)
 	{
-		for (int l=0;l<len;l++)	
+		for (dc=0;dc<dlen;dc++)
 		{
-			DeltaComp = (m*M_PI*lambda0)/gsl_vector_get(lambda,l);
-			DeltaSamp = 2*M_PI*Dn*gsl_vector_get(d,dc)/gsl_vector_get(lambda,l); 
+			for (l=0;l<len;l++)	
+				{
+					DeltaComp = (m*M_PI*lambda0)/gsl_vector_get(lambda,l);
+					DeltaSamp = 2*M_PI*Dn*gsl_vector_get(d,dc)/gsl_vector_get(lambda,l); 
 
-            		//Reflec = [-exp(1i*pi),0;0,-exp(1i*pi)];
+            				//Reflec = [-exp(1i*pi),0;0,-exp(1i*pi)];
 
-			cDC2 = cos(DeltaComp/2);
-			sDC2 = sin(DeltaComp/2);
+					cDC2 = cos(DeltaComp/2);
+					sDC2 = sin(DeltaComp/2);
 		
-			cDS2 = cos(DeltaSamp/2);
-			sDS2 = sin(DeltaSamp/2);
+					cDS2 = cos(DeltaSamp/2);
+					sDS2 = sin(DeltaSamp/2);
 
-			crho = cos(rho);
-			srho = sin(rho);
+					crho = cos(gsl_vector_get(rho,rhol));
+					srho = sin(gsl_vector_get(rho,rhol));
 
-			comp_c[0] = gsl_complex_rect(cDC2,0);
-			comp_c[1] = gsl_complex_rect(0,sDC2);
+					comp_c[0] = gsl_complex_rect(cDC2,0);
+					comp_c[1] = gsl_complex_rect(0,sDC2);
 			
-			gsl_matrix_complex_set(Comp,0,0,comp_c[0]);
-			gsl_matrix_complex_set(Comp,0,1,comp_c[1]);
-			gsl_matrix_complex_set(Comp,1,0,comp_c[1]);
-			gsl_matrix_complex_set(Comp,1,1,comp_c[0]);
+					gsl_matrix_complex_set(Comp,0,0,comp_c[0]);
+					gsl_matrix_complex_set(Comp,0,1,comp_c[1]);
+					gsl_matrix_complex_set(Comp,1,0,comp_c[1]);
+					gsl_matrix_complex_set(Comp,1,1,comp_c[0]);
+		
+					sample_c[0] = gsl_complex_rect(cDS2,(1-2*pow(srho,2))*sDS2);
+					sample_c[1] = gsl_complex_rect(0,2*crho*srho*sDS2);
+					sample_c[2] = gsl_complex_rect(0,2*crho*srho*sDS2);
+					sample_c[3] = gsl_complex_rect(cDS2,(1-2*pow(crho,2))*sDS2);
 
-			sample_c[0] = gsl_complex_rect(cDS2,(1-2*pow(srho,2))*sDS2);
-			sample_c[1] = gsl_complex_rect(0,2*crho*srho*sDS2);
-			sample_c[2] = gsl_complex_rect(0,2*crho*srho*sDS2);
-			sample_c[3] = gsl_complex_rect(cDS2,(1-2*pow(crho,2))*sDS2);
+					gsl_matrix_complex_set(Sample,0,0,sample_c[0]);
+					gsl_matrix_complex_set(Sample,0,1,sample_c[1]);
+					gsl_matrix_complex_set(Sample,1,0,sample_c[2]);
+					gsl_matrix_complex_set(Sample,1,1,sample_c[3]);
 
-			gsl_matrix_complex_set(Sample,0,0,sample_c[0]);
-			gsl_matrix_complex_set(Sample,0,1,sample_c[1]);
-			gsl_matrix_complex_set(Sample,1,0,sample_c[2]);
-			gsl_matrix_complex_set(Sample,1,1,sample_c[3]);
-
-			//matrix manipulations
-			//%E = P*Sample*A;
+				//matrix manipulations
+				//%E = P*Sample*A;
             		//	E = P*Sample*Reflec*Sample*A;   
-            		//	Ts = E'*E;  careful with complex comps in C++
+            		//	Ts = E'*E;  careful with complex conj in C++
             		//	T(l) = Ts; % generate the spectra 
 
-			gsl_blas_zgemm(CblasNoTrans,CblasNoTrans,one,P,Sample,zero,Temp);  // Temp = P*Sample
-			gsl_blas_zgemv(CblasNoTrans,one,Temp,A,zero,E);			// E = Temp*A
+					gsl_blas_zgemm(CblasNoTrans,CblasNoTrans,one,P,Sample,zero,TempA);   // TempA = P*Sample
+					gsl_blas_zgemm(CblasNoTrans,CblasNoTrans,one,TempA,Comp,zero,TempB);  // TempB = TempA*Comp
+					gsl_blas_zgemv(CblasNoTrans,one,TempB,A,zero,E);			  // E = TempB*A
 
-			Ts = gsl_complex_abs2(gsl_vector_complex_get(E,0)); 
+	Ts = gsl_complex_abs2(gsl_vector_complex_get(E,0))+gsl_complex_abs2(gsl_vector_complex_get(E,1)); 
 
-			//printf("Ts %g\n",GSL_REAL(sample_c[0]));
+					xmi = gsl_vector_get(xm,l);	
+					ymi = gsl_vector_get(ym,l);
+					zmi = gsl_vector_get(zm,l);
 
-			xmi = gsl_vector_get(xm,l);	
-			ymi = gsl_vector_get(ym,l);
-			zmi = gsl_vector_get(zm,l);
+					gsl_vector_set(Xr,l,xmi*Ts);
+					gsl_vector_set(Yr,l,ymi*Ts);
+					gsl_vector_set(Zr,l,zmi*Ts);
+				}
 
-			gsl_vector_set(Xr,l,xmi*Ts);
-			gsl_vector_set(Yr,l,ymi*Ts);
-			gsl_vector_set(Zr,l,zmi*Ts);
-		}
-
-		//trapezium method - step is 1nm
-
-		#pragma omp parallel //private(Xr,Yr,Zr)
-  		{
-  			/*tid = omp_get_thread_num();
-  			if (tid == 0)
-    			{
-    				nthreads = omp_get_num_threads();
-    				printf("Number of threads = %d\n", nthreads);
-    			}*/
-
-		#pragma omp sections nowait
-    		{
-    			#pragma omp section
-      			{
+			//trapezium method - step is 1nm
 			X = gsl_blas_dasum(Xr)-(gsl_vector_get(Xr,len-1))/2-(gsl_vector_get(Xr,0))/2;
-			}
-			#pragma omp section
-      			{
 			Y = gsl_blas_dasum(Yr)-(gsl_vector_get(Yr,len-1))/2-(gsl_vector_get(Yr,0))/2;
-			}
-			#pragma omp section
-      			{
-			Z = gsl_blas_dasum(Zr)-(gsl_vector_get(Zr,len-1))/2-(gsl_vector_get(Zr,0))/2;
-			}
-		}
-		} // end of parallel section
+			Z = gsl_blas_dasum(Zr)-(gsl_vector_get(Zr,len-1))/2-(gsl_vector_get(Zr,0))/2;		
 
-		Norm = X+Y+Z;
+			Norm = X+Y+Z;
 		
-		Xn = X/Norm;
-        	Yn = Y/Norm;
-        	Zn = Z/Norm;
+			Xn = X/Norm;
+        		Yn = Y/Norm;
+        		Zn = Z/Norm;
 
-		gsl_vector_set(XYZ,0,Xn);
-		gsl_vector_set(XYZ,1,Yn);
-		gsl_vector_set(XYZ,2,Zn);
+			gsl_vector_set(XYZ,0,Xn);
+			gsl_vector_set(XYZ,1,Yn);
+			gsl_vector_set(XYZ,2,Zn);
 
-		XYZ2RGB(RGB,XYZ); // convert to RGB
+			XYZ2RGB(RGB,XYZ); // convert to RGB
 
-		R = gsl_vector_get(RGB,0);
-		G = gsl_vector_get(RGB,1);
-		B = gsl_vector_get(RGB,2);
-
-		#pragma omp parallel shared(Xmat,Ymat,Zmat,R,G,B) private(yp,tid)
-		{
-
-		/*tid = omp_get_thread_num();
-  			if (tid == 0)
-    			{
-    			nthreads = omp_get_num_threads();
-    			printf("Number of threads = %d\n", nthreads);
-    			}
-  			printf("Thread %d starting...\n",tid);*/
-
-		#pragma omp for schedule(dynamic,ypix)   //chunk is height dimension
-		for(yp=0;yp<ypix;yp++) //will loop on rho for this
-		{		
-			gsl_matrix_set(Xmat,dc,yp,R);
-			gsl_matrix_set(Ymat,dc,yp,G);
-			gsl_matrix_set(Zmat,dc,yp,B);
-			
-			//gsl_matrix_set(Xmat,dc,yp,X);
-			//gsl_matrix_set(Ymat,dc,yp,Y);
-			//gsl_matrix_set(Zmat,dc,yp,Z);	
-		}
-
-		}
+			R = gsl_vector_get(RGB,0);
+			G = gsl_vector_get(RGB,1);
+			B = gsl_vector_get(RGB,2);
+		
+			gsl_matrix_set(Xmat,dc,rhol,R);
+			gsl_matrix_set(Ymat,dc,rhol,G);
+			gsl_matrix_set(Zmat,dc,rhol,B);
+		}	
 	}
 	
 	//free vector and matrix memory
@@ -216,13 +185,18 @@ void DLC::Michel_levy(double Dn, int dlen, int dstart, int dend, gsl_matrix * Xm
 	gsl_vector_free (Yr);
 	gsl_vector_free (Zr);
 
+	gsl_vector_free (d);
+	gsl_vector_free (lambda);
+	gsl_vector_free (rho);
+
 	gsl_vector_complex_free (A);
 	gsl_matrix_complex_free (P);
 
 	gsl_matrix_complex_free (Comp);
 	gsl_matrix_complex_free (Sample);
 	gsl_matrix_complex_free (Reflect);
-	gsl_matrix_complex_free (Temp);
+	gsl_matrix_complex_free (TempA);
+	gsl_matrix_complex_free (TempB);
 
 	gsl_vector_free (XYZ);
 	gsl_vector_free (RGB);
